6c4c6690c53aadcb6ef7770d2ee580a56d5b70f6
THANETNove <novel2020manga@gmail.com>
2024-08-24 18:38:37 +0700

    add

--- resources/views/post_content/create_post.blade.php
+++ resources/views/post_content/create_post.blade.php
@@ -30,12 +30,36 @@
         @endif
 
     </div>
-    <div class="box-justify-content pb-post-19">
+    <div class="box-post-justify-content pb-post-19">
         <img class="pick_highlight_post" id="rectangle123"
             src="{{ URL::asset('/assets/image/welcome/pick_highlight_post.png') }}">
-        <img class="create_new_post" id="rectangle123" src="{{ URL::asset('/assets/image/welcome/create_new_post.png') }}">
+        <img class="create_new_post" id="rectangle123" data-bs-toggle="modal" data-bs-target="#exampleModal"
+            src="{{ URL::asset('/assets/image/welcome/create_new_post.png') }}">
 
         <p class="post">Posts</p>
+    </div>
+
+
+
+    <!-- Modal -->
+    <div class="modal fade " id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h1 class="modal-title-post fs-5" id="exampleModalLabel">สร้างโพส</h1>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body-post">
+                    <div class="mb-3">
+                        <input type="text" class="form-control" name="name" id="exampleFormControlInput1"
+                            placeholder="ชื่อโพส">
+                    </div>
+                    <img class="add-frame7" id="rectangle123" data-bs-toggle="modal" data-bs-target="#exampleModal"
+                        src="{{ URL::asset('/assets/image/welcome/add-frame7.png') }}">
+                </div>
+
+            </div>
+        </div>
     </div>
     <script>
         function goBack() {
