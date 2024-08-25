f1dae65abab58e1a53b26119ae5a391bce70819c
THANETNove <novel2020manga@gmail.com>
2024-08-25 20:14:42 +0700

    add

--- resources/views/co-agent/create_co.blade.php
+++ resources/views/co-agent/create_co.blade.php
@@ -2,11 +2,17 @@
 
 @section('content')
     <div class="box-free-trial">
-        <div class="free-trial-box-nav">
-            <a href="javascript:void(0);" onclick="goBack()">
-                <img class="free-go-back" src="{{ URL::asset('/assets/image/welcome/go-back.png') }}">
-            </a>
-            <p class="free-trial">ฝากทรัพย์</p>
+        <div class="smust-co-head-box free-trial-box-nav">
+            <img class="img-ellipse" src="{{ URL::asset('/assets/image/welcome/Ellipse.png') }}">
+            <div class="back-co">
+                <a href="javascript:void(0);" onclick="goBack()">
+                    <img class="co-go-back" src="{{ URL::asset('/assets/image/welcome/go-back.png') }}">
+                </a>
+                <p class="co-trial">ฝากทรัพย์</p>
+            </div>
+
+
+            <img class="img-smust" src="{{ URL::asset('/assets/image/welcome/SMUSTLogo.png') }}">
         </div>
         <div class="box-announced">
 
