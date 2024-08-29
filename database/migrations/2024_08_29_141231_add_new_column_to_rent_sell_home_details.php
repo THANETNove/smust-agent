5a478e7b330d7d4fad7b2bc2a586d69c69be47e2
THANETNove <novel2020manga@gmail.com>
2024-08-29 22:11:12 +0700

    add

--- database/migrations/2024_08_29_141231_add_new_column_to_rent_sell_home_details.php
+++ database/migrations/2024_08_29_141231_add_new_column_to_rent_sell_home_details.php
@@ -12,28 +12,28 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('rent_sell_home_details', function (Blueprint $table) {
-            $table->string('cross')->nullable()->comment('ข้ามการกรอกข้อมูลใหม');
-            $table->string('sell')->nullable()->comment('ขาย');
-            $table->string('rent')->nullable()->comment('เช่า');
-            $table->string('name_have')->nullable()->comment('โฉนดมีภาระหนี้หรือไม่');
-            $table->string('start_date')->nullable()->comment('เริ่มให้เช่าได้ตั้งแต่');
-            $table->string('road')->nullable()->comment('ถนน');
-            $table->string('alley')->nullable()->comment('ซอย');
-            $table->string('number_parking')->nullable()->comment('จำนวนที่จอดรถ');
-            $table->string('rent_baht_month')->nullable()->comment('ค่าเช่า* (บาท/เดือน)');
-            $table->string('month_advance_rent')->nullable()->comment('ค่าเช่าล่วงหน้า 1 เดือน');
-            $table->string('electricalAppliance')->nullable()->comment('สิ่งอำนวยความสะดวก');
-            $table->string('facilities')->nullable()->comment('เครื่องใช้ไฟฟ้า');
-            $table->string('furniture')->nullable()->comment('เฟอร์นิเจอร์');
-            $table->string('shopping_center')->nullable()->comment('สถานที่สำคัญใกล้เคียง');
-            $table->string('school')->nullable()->comment('สถานศึกษา');
-            $table->string('meters_store')->nullable()->comment('ร้านสะดวกซื้อที่ใกล้ที่สุด');
-            $table->string('url_video')->nullable()->comment('ลิงค์ video ');
-            $table->string('announcement_name')->nullable()->comment('ชื่อประกาศ* ');
-            $table->string('files')->nullable()->comment('files');
-            $table->string('user_name')->nullable()->comment('ชื่อ');
-            $table->string('user_surname')->nullable()->comment('นาสกุล');
-            $table->string('user_phone')->nullable()->comment('เบอร์โทร');
+            $table->string('cross')->nullable()->comment('ข้ามการกรอกข้อมูลใหม่')->after('thereVarious');
+            $table->string('sell')->nullable()->comment('ขาย')->after('cross');
+            $table->string('rent')->nullable()->comment('เช่า')->after('sell');
+            $table->string('name_have')->nullable()->comment('โฉนดมีภาระหนี้หรือไม่')->after('rent');
+            $table->string('start_date')->nullable()->comment('เริ่มให้เช่าได้ตั้งแต่')->after('name_have');
+            $table->string('road')->nullable()->comment('ถนน')->after('start_date');
+            $table->string('alley')->nullable()->comment('ซอย')->after('road');
+            $table->string('number_parking')->nullable()->comment('จำนวนที่จอดรถ')->after('alley');
+            $table->string('rent_baht_month')->nullable()->comment('ค่าเช่า* (บาท/เดือน)')->after('number_parking');
+            $table->string('month_advance_rent')->nullable()->comment('ค่าเช่าล่วงหน้า 1 เดือน')->after('rent_baht_month');
+            $table->string('electricalAppliance')->nullable()->comment('เครื่องใช้ไฟฟ้า')->after('month_advance_rent');
+            $table->string('facilities')->nullable()->comment('สิ่งอำนวยความสะดวก')->after('electricalAppliance');
+            $table->string('furniture')->nullable()->comment('เฟอร์นิเจอร์')->after('facilities');
+            $table->string('shopping_center')->nullable()->comment('สถานที่สำคัญใกล้เคียง')->after('furniture');
+            $table->string('school')->nullable()->comment('สถานศึกษา')->after('shopping_center');
+            $table->string('meters_store')->nullable()->comment('ร้านสะดวกซื้อที่ใกล้ที่สุด')->after('school');
+            $table->string('url_video')->nullable()->comment('ลิงค์ video')->after('meters_store');
+            $table->string('announcement_name')->nullable()->comment('ชื่อประกาศ*')->after('url_video');
+            $table->string('files')->nullable()->comment('ไฟล์')->after('announcement_name');
+            $table->string('user_name')->nullable()->comment('ชื่อ')->after('files');
+            $table->string('user_surname')->nullable()->comment('นามสกุล')->after('user_name');
+            $table->string('user_phone')->nullable()->comment('เบอร์โทร')->after('user_surname');
         });
     }
 
