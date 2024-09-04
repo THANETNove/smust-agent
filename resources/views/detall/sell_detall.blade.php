117d2faa1a1671fa198fe6e3eb3785dde643a8c8
THANETNove <novel2020manga@gmail.com>
2024-09-04 21:24:19 +0700

    add

--- resources/views/detall/sell_detall.blade.php
+++ resources/views/detall/sell_detall.blade.php
@@ -1,5 +1,5 @@
   {{-- ขาย  --}}
-{{--   <p class="text-content">{!! $home->details !!}</p> --}}
+  {{--   <p class="text-content">{!! $home->details !!}</p> --}}
   <div class="mt-3">
       <p class="text-content-black margin-bottom-8">
           <img class="icon-content-2" src="{{ URL::asset('/assets/image/home/pajamas_sort-lowest.png') }}">
@@ -11,10 +11,18 @@
               <p class="text-content-black margin-bottom-8 ">
                   <span>
                       <img class="icon-content-2" src="{{ URL::asset('/assets/image/home/pajamas_sort-lowest.png') }}">
+                      เงินจอง <span class="ml-8"> {{ number_format($home->reservation_amount_baht) }}
+                          บาท</span>
+                  </span>
+              </p>
+              <p class="text-content-black margin-bottom-8 ">
+                  <span>
+                      <img class="icon-content-2" src="{{ URL::asset('/assets/image/home/pajamas_sort-lowest.png') }}">
                       เงินดาวน์ <span class="ml-8"> {{ number_format($home->down_payment) }}
                           บาท</span>
                   </span>
               </p>
+
               @if ($home->down_payment_installments == 'ได้')
                   <p class="text-content-black margin-bottom-8 ">
                       <span><img class="icon-content-2"
@@ -25,20 +33,31 @@
           </div>
           <div class="w-100">
               <p class="text-content-black margin-bottom-8">
-                  @if ($home->down_payment_installments == 'ได้')
+                  @if ($home->down_payment_installments == 'ได้' || $home->installments || $home->each_installment)
                       <img class="icon-content-2" src="{{ URL::asset('/assets/image/home/check.png') }}">
                   @else
                       <img class="icon-content-close" src="{{ URL::asset('/assets/image/home/close.png') }}">
                   @endif
 
-                  ผ่อนดาวน์ {{ $home->down_payment_installments }}
+                  @if ($home->down_payment_installments)
+                      ผ่อนดาวน์ {{ $home->down_payment_installments }}
+                  @endif
+                  @if ($home->installments || $home->each_installment)
+                      ผ่อนดาวน์ ได้
+                  @else
+                      ผ่อนดาวน์ ไม่ได้
+                  @endif
 
               </p>
-              @if ($home->down_payment_installments == 'ได้')
+              @if ($home->down_payment_installments == 'ได้' || $home->installments || $home->each_installment)
+                  <p class="text-content-black margin-bottom-8">
                   <p class="text-content-black margin-bottom-8">
                       <img class="icon-content-2" src="{{ URL::asset('/assets/image/home/pajamas_sort-lowest.png') }}">
-                      งวดละ <span class="ml-16">{{ number_format($home->each_installment) }} บาท
+                      ผ่อนได้ <span class="ml-16">{{ number_format($home->installments) }} งวด/เดือน
                   </p>
+                  <img class="icon-content-2" src="{{ URL::asset('/assets/image/home/pajamas_sort-lowest.png') }}">
+                  งวดละ <span class="ml-16">{{ number_format($home->each_installment) }} บาท
+                      </p>
               @endif
           </div>
       </div>
