2771b937670c77977954bc2f62c1e776d04d1571
THANETNove <novel2020manga@gmail.com>
2024-09-09 08:11:21 +0700

    add

--- resources/views/co-agent/index.blade.php
+++ resources/views/co-agent/index.blade.php
@@ -20,10 +20,61 @@
                         {{--   @if (Carbon\Carbon::parse($home->created_at)->diffInDays(Carbon\Carbon::now()) < 4)
                             <div class="box-new">NEW</div>
                         @endif --}}
-                        <div class="box-img-new">
-                            <img class="img-0831 lazy" src="{{ URL::asset('/img/product/' . $imgUrl[0]) }}">
+
+                        @php
+                            $price = $home->sell_price;
+                            $priceString = (string) $price;
+                            if (strlen($priceString) > 6) {
+                                $priceString = str_replace(',', '', $priceString);
+                                $formattedPrice = number_format($priceString / 1000000, 1) . ' ล้าน';
+                                $price_sell = $formattedPrice;
+                            } else {
+                                $price_sell = number_format($home->sell_price) . ' บาท';
+                            }
+                            $rental_ = $home->rental_price;
+                            $rental_String = (string) $rental_;
+                            if (strlen($rental_String) > 6) {
+                                $rental_String = str_replace(',', '', $rental_String);
+                                $formatted_rental = number_format($rental_String / 1000000, 1) . ' ล้าน';
+                                $rental_price = $formatted_rental;
+                            } else {
+                                $rental_price = number_format($home->rental_price) . ' บาท';
+                            }
+                        @endphp
+
+                        <div class="box-img-new2">
+                            <img class="img-0831 lazy mb-2" src="{{ URL::asset('/img/product/' . $imgUrl[0]) }}">
+                            @if ($home->rent_sell == 'เช่า')
+                                <span class="rent-sell-primary absolute-rent-sell">{{ $home->rent_sell }}</span>
+                            @elseif ($home->rent_sell == 'ขาย')
+                                <span class="rent-sell-yellow absolute-rent-sell">{{ $home->rent_sell }}</span>
+                            @elseif ($home->rent_sell == 'เช่า/ขาย' || $home->rent_sell == 'เช่าซื้อ/ขายผ่อน')
+                                <span class="rent-sell-green absolute-rent-sell">{{ $home->rent_sell }}</span>
+                            @endif
+
+                            @if ($home->rent == 'เช่า')
+                                <span class="rent-sell-primary absolute-rent-sell">{{ $home->rent }}</span>
+                            @endif
+
+                            @if ($home->sell == 'ขาย')
+                                <span class="rent-sell-yellow absolute-rent-sell">{{ $home->sell }}</span>
+                            @endif
                         </div>
                         <div class="box-name-new">
+                            @if (($home->sell_price && $home->rent_sell == 'เช่า/ขาย') || $home->rent_sell == 'เช่าซื้อ/ขายผ่อน')
+                                <p class="price-new">฿
+                                    {{ number_format($home->rental_price) }}/m
+                                </p>
+                                <p class="price-new price-top-sell2">฿ {{ $price_sell }}</p>
+                            @else
+                                @if (($home->rental_price && $home->rent_sell == 'เช่า') || $home->rent == 'เช่า')
+                                    <p class="price-new">฿ {{ number_format($home->rental_price) }}/m
+                                    </p>
+                                @endif
+                                @if (($home->sell_price && $home->rent_sell == 'ขาย') || $home->sell == 'ขาย')
+                                    <p class="price-new">฿{{ $price_sell }}</p>
+                                @endif
+                            @endif
                             <p class="name-content">{{ $home->building_name }}</p>
                             <p class="name-details">
                                 <img class="img-icon " src="{{ URL::asset('/assets/image/home/location_on.png') }}">
@@ -53,65 +104,6 @@
                             </p>
                         </div>
 
-                        @php
-                            $price = $home->sell_price;
-                            $priceString = (string) $price;
-                            if (strlen($priceString) > 6) {
-                                $priceString = str_replace(',', '', $priceString);
-                                $formattedPrice = number_format($priceString / 1000000, 1) . ' ล้าน';
-                                $price_sell = $formattedPrice;
-                            } else {
-                                $price_sell = number_format($home->sell_price) . ' บาท';
-                            }
-                            $rental_ = $home->rental_price;
-                            $rental_String = (string) $rental_;
-                            if (strlen($rental_String) > 6) {
-                                $rental_String = str_replace(',', '', $rental_String);
-                                $formatted_rental = number_format($rental_String / 1000000, 1) . ' ล้าน';
-                                $rental_price = $formatted_rental;
-                            } else {
-                                $rental_price = number_format($home->rental_price) . ' บาท';
-                            }
-                        @endphp
-
-
-                        <div class="box-width-rent-sell">
-                            @if ($home->rent_sell == 'เช่า')
-                                <span class="rent-sell-primary absolute-rent-sell">{{ $home->rent_sell }}</span>
-                            @elseif ($home->rent_sell == 'ขาย')
-                                <span class="rent-sell-yellow absolute-rent-sell">{{ $home->rent_sell }}</span>
-                            @elseif ($home->rent_sell == 'เช่า/ขาย' || $home->rent_sell == 'เช่าซื้อ/ขายผ่อน')
-                                <span class="rent-sell-green absolute-rent-sell">{{ $home->rent_sell }}</span>
-                            @endif
-
-                            @if ($home->rent == 'เช่า')
-                                <span class="rent-sell-primary absolute-rent-sell">{{ $home->rent }}</span>
-                            @endif
-
-                            @if ($home->sell == 'ขาย')
-                                <span class="rent-sell-yellow absolute-rent-sell">{{ $home->sell }}</span>
-                            @endif
-
-                            <div class="box-price-new">
-
-                                @if (($home->sell_price && $home->rent_sell == 'เช่า/ขาย') || $home->rent_sell == 'เช่าซื้อ/ขายผ่อน')
-                                    <p class="price-new">฿
-                                        {{ number_format($home->rental_price) }}/m
-                                    </p>
-                                    <p class="price-new price-top-sell2">฿ {{ $price_sell }}</p>
-                                @else
-                                    @if (($home->rental_price && $home->rent_sell == 'เช่า') || $home->rent == 'เช่า')
-                                        <p class="price-new">฿ {{ number_format($home->rental_price) }}/m
-                                        </p>
-                                    @endif
-                                    @if (($home->sell_price && $home->rent_sell == 'ขาย') || $home->sell == 'ขาย')
-                                        <p class="price-new">฿{{ $price_sell }}</p>
-                                    @endif
-                                @endif
-
-
-                            </div>
-                        </div>
                     </div>
                 </a>
             @endforeach
