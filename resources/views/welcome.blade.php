30d42da2fa9f5f8c69139cb93e79dfa98ade3873
THANETNove <novel2020manga@gmail.com>
2024-09-11 20:57:42 +0700

    add

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -11,27 +11,17 @@
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav me-auto mb-2 mb-lg-0 navbar-nav-white">
                     <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="#">Home</a>
+                        <a class="nav-link active" aria-current="page" href="#">หาบ้าน/คอนโดที่ถูกใจ</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="#">Link</a>
-                    </li>
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
-                            aria-expanded="false">
-                            Dropdown
-                        </a>
-                        <ul class="dropdown-menu">
-                            <li><a class="dropdown-item" href="#">Action</a></li>
-                            <li><a class="dropdown-item" href="#">Another action</a></li>
-                            <li>
-                                <hr class="dropdown-divider">
-                            </li>
-                            <li><a class="dropdown-item" href="#">Something else here</a></li>
-                        </ul>
+                        <a class="nav-link active" aria-current="page" href="#">ศูนย์รวมนายหน้าฝีมือดี</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link disabled" aria-disabled="true">Disabled</a>
+                        <a class="nav-link" href="#">สมัครงานกับเรา</a>
+                    </li>
+
+                    <li class="nav-item">
+                        <a class="nav-link" aria-disabled="true">เจ้าของให้เราช่วยขายได้ ฟรี</a>
                     </li>
                 </ul>
             </div>
