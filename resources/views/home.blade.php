32d1cfcbff134f7bc5db9781dcf687335d6fb382
THANETNove <novel2020manga@gmail.com>
2024-11-10 14:47:40 +0700

    add

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,7 +6,8 @@
         $currentDate = Carbon\Carbon::now(); // วันและเวลาปัจจุบัน
         $userCreatedDate = Carbon\Carbon::parse(Auth::user()->created_at); // วันที่ของผู้ใช้
         $createdDate = $userCreatedDate->lessThan($currentDate->subDays(3));
-        $authCount = Auth::user()->plans == 0 && $createdDate ? 1 : 2;
+        $authCount = Auth::user()->plans > 1 && $createdDate ? 2 : 1;
+
     @endphp
 
 
